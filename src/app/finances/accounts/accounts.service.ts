ba749445752c9b70bdaa5b49f9a2db5c105b6d0b
Jonatas Serra <jonatasserra@outlook.com>
2024-06-13 16:14:19 -0300

    alteracao

--- src/app/finances/accounts/accounts.service.ts
+++ src/app/finances/accounts/accounts.service.ts
@@ -1,6 +1,6 @@
 import { forwardRef, Inject, Injectable } from '@nestjs/common'
 import { InjectModel } from '@nestjs/mongoose'
-import { Model, Types } from 'mongoose'
+import { Model } from 'mongoose'
 import { TransactionService } from '../transactions/transactions.service'
 import { Account, AccountDocument } from './entities/account.entity'
 import { CreateAccountDto } from './dto/create-account.dto'
